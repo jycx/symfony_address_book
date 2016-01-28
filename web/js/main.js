9a6484243423f5d30ccefc852a00f1864a7a6209
jycx <2257289265@qq.com>
2016-01-28 21:08:51 +0800

    midify the home page

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,12 @@
  var count=1;
 function change(){
   setTimeout('change()',3000);
-  var img=document.getElementById("img").src="../img/"+count+".jpg";
+  var img1=document.getElementById("img1").src="../img/"+count+".jpg";
+  var a=count+1;
+  var img2=document.getElementById("img2").src="../img/"+a+".jpg";
+  
+   var img3=document.getElementById("img3").src="../img/"+a+".jpg";
+  var img4=document.getElementById("img4").src="../img/"+count+".jpg";
      count++;
    if(count>3)
     count=1;
